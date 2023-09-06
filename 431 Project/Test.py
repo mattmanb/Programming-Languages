085e0ac504532e19a3a8ccbfabf7a48723c0a1c0
Matt Barrington <mattbarrington7@gmail.com>
2023-09-06 12:55:11 -0400

    Got rid of presentation images

--- 431 Project/Test.py
+++
@@ -1,6 +0,0 @@
-#this creates an array object called my_arr with no values
-my_arr = []
-#this fills my_arr with range(5) values, which is 0-4
-my_arr = [i for i in range(5)]
-#this prints the entire array inside of square brackets 
-print(my_arr)
